b3f81c1730badd132043bac9d623b05852207dc4
TheVannysher | Albert Jetté <armadwarf@hotmail.com>
2023-10-10 12:04:51 -0400

    empty scss

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -5,18 +5,17 @@ import { BOTTOM_NAV_ROUTES, ROUTES_PATH } from 'src/lib/enum/routes';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.components.scss'],
 })
 export class AppComponent implements OnInit {
   showBottomNav: boolean = false;
   router:Router = inject(Router);
 
   ngOnInit() {
-    console.log(this.router.events.subscribe((e) => {
+    this.router.events.subscribe((e) => {
       if (e.type == EventType.NavigationEnd) {
         const { url } = e;
         this.showBottomNav = BOTTOM_NAV_ROUTES.includes(url.split('/')[1] as ROUTES_PATH);
       } 
-    }));
+    });
   }
 }
